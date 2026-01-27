ca8fba82dfe32a37de69304a0ca0d3566a278bf8
leobidoous <leobidoous@icloud.com>
2026-01-27 17:11:37 -0300

    feat: upgrade dependencies

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,6 @@ import Foundation
 import device_info_plus
 import file_picker
 import file_selector_macos
-import path_provider_foundation
 import sqflite_darwin
 import syncfusion_pdfviewer_macos
 import url_launcher_macos
@@ -18,7 +17,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   SyncfusionFlutterPdfViewerPlugin.register(with: registry.registrar(forPlugin: "SyncfusionFlutterPdfViewerPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
--- lib/src/presentation/controllers/file_picker_controller.dart
+++ lib/src/presentation/controllers/file_picker_controller.dart
@@ -3,40 +3,35 @@ import 'package:file_picker/file_picker.dart' show FilePicker, FileType;
 import '../../../base_style_sheet.dart' show PlatformFile;
 
 class FilePickerController {
-  final _platform = FilePicker.platform;
-
   String filePath = '';
 
   Future<String> pickFilePath({
     FileType fileType = FileType.custom,
     List<String> allowedExtensions = const ['pdf'],
   }) async {
-    return _platform
-        .pickFiles(
-          type: fileType,
-          compressionQuality: 10,
-          allowedExtensions: allowedExtensions,
-        )
-        .then((onValue) {
-          if ((onValue?.count ?? 0) <= 0) return '';
+    return FilePicker.pickFiles(
+      type: fileType,
+      compressionQuality: 10,
+      allowedExtensions: allowedExtensions,
+    ).then((onValue) {
+      if ((onValue?.count ?? 0) <= 0) return '';
 
-          return onValue?.files.first.path ?? '';
-        });
+      return onValue?.files.first.path ?? '';
+    });
   }
+
   Future<List<PlatformFile>> pickFile({
     FileType fileType = FileType.custom,
     List<String> allowedExtensions = const ['pdf'],
   }) async {
-    return _platform
-        .pickFiles(
-          type: fileType,
-          compressionQuality: 10,
-          allowedExtensions: allowedExtensions,
-        )
-        .then((onValue) {
-          if ((onValue?.count ?? 0) <= 0) return const [];
+    return FilePicker.pickFiles(
+      type: fileType,
+      compressionQuality: 10,
+      allowedExtensions: allowedExtensions,
+    ).then((onValue) {
+      if ((onValue?.count ?? 0) <= 0) return const [];
 
-          return onValue?.files ?? const [];
-        });
+      return onValue?.files ?? const [];
+    });
   }
 }
