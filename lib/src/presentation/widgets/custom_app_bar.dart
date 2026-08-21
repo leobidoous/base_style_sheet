61254405493ff113ec6ed5075003b6837463a7e7
leobidoous <leobidoous@icloud.com>
2026-08-21 00:21:19 -0300

    wip: update appbar

--- lib/src/presentation/widgets/custom_app_bar.dart
+++ lib/src/presentation/widgets/custom_app_bar.dart
@@ -205,7 +205,7 @@ class _CustomAppBarState extends State<CustomAppBar>
             },
           ),
         ),
-        if (widget.progress != null)
+        if (widget.progress != null && (widget.toolbarHeight ?? 0) > 0)
           Padding(
             padding: widget.linearProgressPadding ?? .zero,
             child: ClipRRect(
